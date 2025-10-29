f68ee40cef80b432bd069fb84ef8a9649f143332
santiago343gb <xantiago.spa@gmail.com>
2025-10-29 20:33:56 +0100

    Update __manifest__.py

---
+++ extra-addons/dummy_module/__manifest__.py
@@ -0,0 +1,13 @@
+nombreAlumno = "Carlos"
+edadAlumno = 20
+
+c
+print("Nombre del alumno:", nombreAlumno)
+print("Edad del alumno:", edadAlumno)
+
+
+nombrePersona = input("Ingresa tu nombre: ")
+edadPersona = input("Ingresa tu edad: ")
+
+print("Tu nombre es:", nombrePersona)
+print("Tu edad es:", edadPersona)
